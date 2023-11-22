eb64f15acfd78d3503b03f7bf208610ea99db668
John Diginee <Johndbizz@gmail.com>
2023-11-22 11:31:29 +0100

    Chore: Update static files

--- core/settings.py
+++ core/settings.py
@@ -140,11 +140,15 @@ USE_TZ = True
 # STATIC_ROOT = 'static'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'static_root'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
 
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
+
+if os.environ.get('ENV') == 'PRODUCTION':
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
